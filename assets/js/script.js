7dd0cd01dc71c3d9a9e134f71611b7a785980bd4
Smoom <david.tait.96@gmail.com>
2021-11-20 13:33:38 -0700

    questions all display now

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,36 @@ var startButton = document.querySelector('.start');
 var highScoresButton = document.querySelector('.high-score');
 var timerEl = document.querySelector('.timer');
 var mainEl = document.querySelector('.main');
+var questionEl = document.querySelector('.question');
+var answersEl = document.querySelector('.answers');
 var score = 0;
+var currentQuestion = 0;
+var timeLeft = 60;
 
 var questions = [ {
   question: "What is hutch's favorite color?",
   answer: ["blue", "green", "black", "yellow"],
   correct: 0
-}]
+},
+
+{
+  question: "Does hutch like wet or dry food?",
+  answer: ["Wet", "Dry", "Both", "Neither"],
+  correct: 1
+},
+{
+  question: "What is hutch's favorite color?",
+  answer: ["blue", "green", "black", "yellow"],
+  correct: 0
+},
+{
+  question: "Does hutch like wet or dry food?",
+  answer: ["Wet", "Dry", "Both", "Neither"],
+  correct: 1
+}
+];
 
 var startGame = function() {
-  var timeLeft = 60;
   startButton.remove();
   // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function() {
@@ -23,48 +43,63 @@ var startGame = function() {
     }
   }, 1000);
 
-  displayQuestion(1);
-
-
-
+  displayQuestion(currentQuestion); 
 };
 
-var displayQuestion = function(num) {
-  var questionAreaEl = document.createElement("div");
-  questionAreaEl.classList.add("questionArea" + num);
-  var questionTitleEl = document.createElement("h2");
-  questionTitleEl.textContent = "Question " + num + " : " + questions[num-1].question;
+var deleteQuiz = function(num) {
+  var questionChildCount = questionEl.childElementCount;
+  for(var i=0; i < questionChildCount; i++){
+    questionEl.removeChild(questionEl.childNodes[0]);
+    console.log(questionEl);
+  }
 
-  orderedListEl = document.createElement("ol");
+  var answersChildCount = answersEl.childElementCount;
+  if(num === 0){
+    answersEl.removeChild(answersEl.childNodes[0]);
+  }
+  for(var j=0; j < answersChildCount; j++){
+    console.log("answers childnode0", answersEl.childNodes[0]);
+    answersEl.removeChild(answersEl.childNodes[0]);
+  }
+
+  questionEl.classList.remove("questionArea"+num);
+}
+
+var displayQuestion = function(num) {
+  questionEl.classList.add("questionArea" + num);
+  var questionTitleEl = document.createElement("h2");
+  questionTitleEl.textContent = "Question " + (num+1) + " : " + questions[num].question;
+
 
   for(var i=0; i < 4; i++){
     var answer = document.createElement("li");
     answer.classList.add("answer"+i);
-    answer.textContent = questions[num-1].answer[i];
-    orderedListEl.appendChild(answer);
+    answer.textContent = questions[num].answer[i];
+    answersEl.appendChild(answer);
   }
 
-  questionAreaEl.appendChild(questionTitleEl);
-  questionAreaEl.appendChild(orderedListEl);
-  mainEl.appendChild(questionAreaEl);
+  questionEl.appendChild(questionTitleEl);
+  questionEl.appendChild(answersEl);
 
-  questionAreaEl.addEventListener("click", function(event) {
-    var element = event.target;
-    if(element.getAttribute("class") === ("answer"+questions[num-1].correct)){
-      console.log("you got the answer right");
-      return true;
-    } else {
-      console.log("you got the answer wrong");
-      return false;
-      
-    }
-  });
-
-
-
-  console.log(mainEl);
 }
 
 
 
 startButton.addEventListener("click", startGame);
+
+questionEl.addEventListener("click", function(event){  
+  var element = event.target;
+  if(element.getAttribute("class") === ("answer"+questions[currentQuestion].correct)){
+    console.log("you got the answer right");
+    score += 1;
+  } else {
+    console.log("you got the answer wrong");
+    timeLeft -= 10;
+  }
+
+  if(currentQuestion < questions.length){
+    deleteQuiz(currentQuestion);
+    currentQuestion++;
+    displayQuestion(currentQuestion);
+  }
+});
