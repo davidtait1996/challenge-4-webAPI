c2c190f668707d8410e1cb81a25c2ba1350c1b82
Smoom <david.tait.96@gmail.com>
2021-11-21 12:53:27 -0700

    local storage, printing some more stuff to screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,11 @@ var timerEl = document.querySelector('.timer');
 var mainEl = document.querySelector('.main');
 var questionEl = document.querySelector('.question');
 var answersEl = document.querySelector('.answers');
+var resultEl = document.querySelector('.result');
+var scoreEl = document.querySelector('.score');
 var score = 0;
 var currentQuestion = 0;
-var timeLeft = 60;
+var timeLeft = 20;
 
 var questions = [ {
   question: "What is hutch's favorite color?",
@@ -39,18 +41,20 @@ var startGame = function() {
       timerEl.textContent = "Timer : " + timeLeft;
       timeLeft--;
     } else {
-
+      clearInterval(timeInterval);
+      timerEl.remove();
     }
   }, 1000);
 
   displayQuestion(currentQuestion); 
+
+  scoreEl.textContent = "Score: " + score;
 };
 
 var deleteQuiz = function(num) {
   var questionChildCount = questionEl.childElementCount;
   for(var i=0; i < questionChildCount; i++){
     questionEl.removeChild(questionEl.childNodes[0]);
-    console.log(questionEl);
   }
 
   var answersChildCount = answersEl.childElementCount;
@@ -58,7 +62,6 @@ var deleteQuiz = function(num) {
     answersEl.removeChild(answersEl.childNodes[0]);
   }
   for(var j=0; j < answersChildCount; j++){
-    console.log("answers childnode0", answersEl.childNodes[0]);
     answersEl.removeChild(answersEl.childNodes[0]);
   }
 
@@ -68,6 +71,7 @@ var deleteQuiz = function(num) {
 var displayQuestion = function(num) {
   questionEl.classList.add("questionArea" + num);
   var questionTitleEl = document.createElement("h2");
+  console.log(num);
   questionTitleEl.textContent = "Question " + (num+1) + " : " + questions[num].question;
 
 
@@ -87,19 +91,31 @@ var displayQuestion = function(num) {
 
 startButton.addEventListener("click", startGame);
 
-questionEl.addEventListener("click", function(event){  
+answersEl.addEventListener("click", function(event){  
   var element = event.target;
   if(element.getAttribute("class") === ("answer"+questions[currentQuestion].correct)){
     console.log("you got the answer right");
     score += 1;
+    scoreEl.textContent = "Score: " + score;
+    resultEl.textContent = "You got the answer right";
   } else {
     console.log("you got the answer wrong");
     timeLeft -= 10;
+    resultEl.textContent = "You got the answer wrong";
   }
+  console.log(score);
 
-  if(currentQuestion < questions.length){
+
+  console.log("length", questions.length);
+  if(currentQuestion < questions.length-1){
     deleteQuiz(currentQuestion);
     currentQuestion++;
+    console.log("current question", currentQuestion);
     displayQuestion(currentQuestion);
+  } else if ((currentQuestion === questions.length-1) || (timeLeft === 0)) {
+    localStorage.setItem("highScore", score);
+    questionEl.remove();
+    answersEl.remove();
+    timerEl.remove();
   }
 });
